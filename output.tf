bc02670ded9779aa72c90a3cd1e446f3b454b7b7
sairamavvaru <sairamavvaru203@gmail.com>
2025-07-04 01:05:00 +0530

    update code output file

--- output.tf
+++ output.tf
@@ -1,4 +1,7 @@
 output "instance_public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_instance.web.public_ip
+  value = aws_instance.web.public_ip
+}
+
+output "instance_public_sg" {
+  value = aws_instance.web.security_groups[0]
 }
